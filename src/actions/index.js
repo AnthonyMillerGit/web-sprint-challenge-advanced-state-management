8dbadcdef0dcd901017dc168f88b3d8d475bf550
ferengitooth <ehitsamill@gmail.com>
2021-07-16 11:45:51 -0700

    set up most, if not all of the actions index.js

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,36 @@
 import axios from 'axios';
 
+
+export const fetchSmurfs = () => {
+    return dispatch => {
+        dispatch({type: LOAD_SMURFS})
+
+        axios.get("http://localhost:3333/smurfs")
+        .then(res => {
+            dispatch({type: DISPLAY_SMURFS, payload: res.data})
+        })
+        .catch(error => {
+            dispatch({type: MISSING_SMURFS, payload: error.Response.code})
+        })
+    }
+}
+
+const newSmurf = []
+
+export const addSmurf = smurf => {
+    return dispatch => {
+        axios.post("http://localhost:3333/smurfs", smurf)
+        .then(res => {
+            dispatch({type: ADD_SMURF, payload: smurf})
+        })
+        .catch(err => {
+            console.log(err)
+        })
+    }
+}
+
+
+
 //Task List:
 //1. Add a thunk action called fetchSmurfs that triggers a loading status display in our application, performs an axios call to retreive smurfs from our server, saves the result of that call to our state and shows an error if one is made.
 //2. Add a standard action that allows us to add new smurf (including the name, nickname, position, summary)
