5c4f261e6b353bfe05d049cedb8548cdd39e2224
ferengitooth <ehitsamill@gmail.com>
2021-07-16 12:11:52 -0700

    swapped App. js out from class component App is now also fully fleshed out HIT MVP

--- src/App.js
+++ src/App.js
@@ -1,28 +1,42 @@
-import React, { Component } from "react";
-
+import React, { Component, useEffect } from "react";
+import {connect} from 'react-redux'
 import AddForm from './components/AddForm';
 import SmurfList from './components/SmurfList';
 import Header from './components/Header';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import "./App.css";
+import {fetchSmurfs} from './actions/index'
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <Header />
 
-        <main>
-          <SmurfList/>
-          <AddForm/>
-        </main>
-      </div>
-    );
+const App = (props) => {
+
+  useEffect(() => {
+    props.fetchSmurfs()
+  },[])
+
+  return (
+    <div className="App">
+      <Header />
+
+      <main>
+        <SmurfList/>
+        <AddForm/>
+      </main>
+    </div>
+  );
+
+}
+
+const mapStateToProps = state => {
+  return {
+    smurfs: state.smurfs,
+    isLoading: state.isLoading,
+    error: state.error
   }
 }
 
-export default App;
+export default connect(mapStateToProps, {fetchSmurfs})(App)
 
 //Task List:
 //1. Connect the fetchSmurfs actions to the App component.
